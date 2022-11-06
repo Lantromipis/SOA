1d443392c5771e7aae461be0f8cd6ac4e9c697c9
Afomin <284718@niuitmo.ru>
2022-11-06 20:44:33 +0300

    Lab #3 Added Eureka server

--- second-service-rest/src/main/java/se/ifmo/ru/SpringMain.java
+++ second-service-rest/src/main/java/se/ifmo/ru/SpringMain.java
@@ -2,8 +2,10 @@ package se.ifmo.ru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringMain {
     public static void main(String[] args) {
         SpringApplication.run(SpringMain.class, args);
